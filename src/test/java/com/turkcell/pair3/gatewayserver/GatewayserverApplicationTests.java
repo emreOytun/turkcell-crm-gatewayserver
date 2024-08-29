f22c0c618cceedc690dae7f2c4644f11f5339e7e
emreOytun <emreoytun58@gmail.com>
2024-08-29 21:45:51 +0300

    config class for test class added

--- src/test/java/com/turkcell/pair3/gatewayserver/GatewayserverApplicationTests.java
+++ src/test/java/com/turkcell/pair3/gatewayserver/GatewayserverApplicationTests.java
@@ -1,9 +1,10 @@
 package com.turkcell.pair3.gatewayserver;
 
+import com.turkcell.gatewayserver.GatewayserverApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GatewayserverApplication.class)
 class GatewayserverApplicationTests {
 
 	@Test
